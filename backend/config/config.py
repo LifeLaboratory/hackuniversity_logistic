03dac573c36f197f4ae757941641620e29eefeaa
raldenprog <raldenprog@mail.ru>
2019-03-22 14:31:06 +0300

    server ip

--- backend/config/config.py
+++ backend/config/config.py
@@ -2,7 +2,7 @@ HEADER = {'Access-Control-Allow-Origin': '*'}
 DATABASE = {
     "dbname": "hackuniversity_logist",
     "user": "inside_user",
-     "host": "90.189.132.25",
-    #"host": "192.168.1.3",
+    #"host": "90.189.132.25",
+    "host": "192.168.0.13",
     "password": "PINLOX!@#PINLOX!@#"
 }
